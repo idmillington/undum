a00f74e6a8978690ee441020bb03eb54d69ee0eb
Oreolek <oreolek@gmail.com>
2010-06-20 08:26:59 +0700

    Made localization. There is another way to do it - using CSS, we could just switch the right strings to visible. It's fast and good - but it bloats the code. I mean, REALLY bloats. Imagine 32 strings instead of every one. So - that's it. Undum is, finally, international.

---
+++ games/media/js/strings.js
@@ -0,0 +1,47 @@
+/* This is the strings file. It contains ALL the strings of the Undum core.
+ * If you want to translate Undum to another language, use this file to create one.
+ * This is convenient so the localization would be apart of the core mechanics. - Oreolek */
+
+Globalite.language("ru", {
+ terrible: "ужасно",
+ poor: "бедно",
+ mediocre: "средненько",
+ fair: "нормально",
+ good: "хорошо",
+ great: "прекрасно",
+ superb: "великолепно",
+ yes: "да",
+ no: "нет",
+ no_group_definition: "Невозможно найти определение группы {id}.",
+ link_not_valid: "Ссылка '{link}' не выглядит правильной.",
+ link_no_action: "Ссылка с ситуацией '.' должна иметь действие.",
+ unknown_situation: "Вы не можете переместиться в неизвестную ситуацию {id}.",
+ erase_message: "Это навсегда удалит вашего персонажа и немедленно вернёт вас к началу игры. Вы уверены?",
+ no_current_situation: "Я не могу отобразить, потому что у нас нет текущей ситуации.",
+ no_local_storage: "Локальное хранилище недоступно.",
+ random_seed_error: "Вы должны задать верное случайное зерно.",
+ random_error: "Проинициализируйте Random с непустым зерном перед использованием.",
+ dice_string_error: "не могу интерпретировать вашу строку кубиков: '{string}'."
+});
+
+Globalite.language("en", {
+ terrible: "terrible",
+ poor: "poor",
+ mediocre: "mediocre",
+ fair: "fair",
+ good: "good",
+ great: "great",
+ superb: "superb",
+ yes: "yes",
+ no: "no",
+ no_group_definition: "Couldn't find a group definition for {id}.",
+ link_not_valid: "The link '{link}' doesn't appear to be valid.",
+ link_no_action: "A link with a situation of '.', must have an action.",
+ unknown_situation: "You can't move to an unknown situation: {id}.",
+ erase_message: "This will permanently delete this character and immediately return you to the start of the game. Are you sure?",
+ no_current_situation: "I can't display, because we don't have a current situation.",
+ no_local_storage: "No local storage available.",
+ random_seed_error: "You must provide a valid random seed.",
+ random_error: "Initialize the Random with a non-empty seed before use.",
+ dice_string_error: "Couldn't interpret your dice string: '{string}'."
+});
